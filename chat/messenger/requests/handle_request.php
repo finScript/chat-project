e31e0af17663f624f7478895792de1b7f073df53
--global <elias.nieminen@gmail.com>
2014-08-12 16:48:29 +0300

    Requests ready

--- chat/messenger/requests/handle_request.php
+++ chat/messenger/requests/handle_request.php
@@ -11,6 +11,8 @@
 	$user = unserialize($_SESSION['user']);
 	$db = new mysqli(_host, _user, _pass, _dbname);
 	
+	$t = date('H') . ":" . date('i') . ":" . date('s');
+	
 	$a = $_POST['a'];
 	
 	if($a == "accept") {
@@ -18,6 +20,48 @@
 		$u = $_POST['u'];
 		$sql = "SELECT * FROM participants WHERE username = '" . $u . "'";
 		$res = $db->query($sql);
+		$u_chatkey = $res->fetch_object()->chatkey;
+		
+		if($res->num_rows) {
+			
+			if($user->access_to != $u_chatkey) {
+				
+				echo "This person is already participating another chat session. This request will be removed.";
+				remove_request($u, $db);
+				die();
+				
+			} else {
+				
+				echo "This person is already participating this chat session. This request will be removed.";
+				remove_request($u, $db);
+				die();
+				
+			}
+			
+		} else {
+			
+			$sql = "SELECT * FROM active_chats WHERE host ='" . $u . "'";
+			if($res->num_rows) {
+				
+				echo "This person is already hosting another chat session. This request will be removed.";
+				remove_request($u, $db);
+				die();
+				
+			} else {
+				
+				$sql_events = "INSERT INTO events(event_id, chatkey, username, occurred) VALUES (3, '" . $user->access_to . "', '$u', '$t')";
+				$sql_participants = "INSERT INTO participants(username, chatkey) VALUES ('$u', '" . $user->access_to . "')";
+				
+				$db->query($sql_events);
+				$db->query($sql_participants);
+				
+				remove_request($u, $db);
+				die("0");
+				
+			}
+			
+		}
+		
 		
 		
 	} elseif($a == "decline") {
@@ -26,7 +70,12 @@
 		
 	}
 	
-	
+	function remove_request($req_user, $_db) {
+		
+		$sql = "DELETE FROM requests WHERE user_from = '" . $req_user . "'";
+		$_db->query($sql);
+		
+	}
 	
 	
 ?>
